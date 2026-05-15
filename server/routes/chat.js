9bc4f8531ab1978b5629104ed724b4a3f83b28c7
Shubham Choudhary <shubham92975@gmail.com>
2026-05-15 14:11:55 +0530

    Fix import case for authMiddleware

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getChatHistory, saveMessage, getBuyersForBook } = require('../controllers/chatController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authmiddleware');
 
 router.get('/book/:bookId/buyers', protect, getBuyersForBook);
 router.get('/:roomId', protect, getChatHistory);
